76ae20cbf887a2683b7828307d17c7a2a5665b7b
rajkumar27 <49371729+rajkumar27@users.noreply.github.com>
2021-08-22 12:36:49 +0530

    Added verify Account and Account exist

--- 3_Implementation/src/masterAccountFunctions.c
+++ 3_Implementation/src/masterAccountFunctions.c
@@ -217,3 +217,75 @@ status modifyMasterPassword(const char *new_password)
 
     return return_value;
 }
+
+bool verifyMasterUserAccount(const char *username, const char *password)
+{
+    masterAccount userInput, masterUser; // for user enterd credentials and credentials saved in USER_FILE
+    FILE *infile;                        // to read USER_FILE
+    SHA256_CTX sha256;                   // For Hashing
+
+    if (username == NULL || password == NULL)
+    {
+        return false;
+    }
+
+    if (strlen(username) == 0 || strlen(password) == 0)
+    {
+        return EMPTY_STRING;
+    }
+
+    //1. create hash of username entered by user
+    sha256_init(&sha256);
+    sha256_update(&sha256, username, strlen(username));
+    sha256_final(&sha256, userInput.username);
+
+    //2. create hash of password entered by user
+    sha256_init(&sha256);
+    sha256_update(&sha256, password, strlen(password));
+    sha256_final(&sha256, userInput.password);
+
+    // EVP_Digest(username, strlen(username), userInput.username, NULL, EVP_sha256(), NULL);
+
+    // EVP_Digest(password, strlen(password), userInput.password, NULL, EVP_sha256(), NULL);
+
+    //3. open USER_FILE
+    infile = fopen(USER_FILE, "r");
+    if (infile == NULL)
+    {
+        printf("%s\n", "Failed to open USER_FILE");
+        return FAILURE;
+    }
+
+    //4. Read USER_FILE
+    fread(&masterUser, sizeof(masterAccount), 1, infile);
+
+    //5. Compare hashes of USER_FILE Credentials and Credentials entered by User
+    if (memcmp(&userInput, &masterUser, sizeof(masterUser)) == 0)
+    {
+        // If Credential match
+        return true;
+    }
+
+    // 6. close the infile
+    fclose(infile);
+
+    return false;
+
+bool masterUserAccountExist()
+{
+
+    //1. Check if USER_FILE exist then master account Exist
+    if (access(USER_FILE, F_OK) == 0)
+    {
+        return true;
+    }
+
+    // 2. if no USER_FILE exist then check if credential file exist , if yes delete it
+    if (access(CREDENTIAL_FILE, F_OK) == 0)
+    {
+        remove(CREDENTIAL_FILE);
+    }
+
+    // 3. Master account do not exist
+    return false;
+}
