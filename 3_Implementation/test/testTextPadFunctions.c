6b97144e71a95f5a7271b9b8289a84356a73706c
KubasadSumanth <86420140+KubasadSumanth@users.noreply.github.com>
2021-08-22 10:02:46 +0530

    Test file created
    Test file created

---
+++ 3_Implementation/test/testTextPadFunctions.c
@@ -0,0 +1,58 @@
+#include "unity.h"
+#include "passwordUsernameFormatChecker.h"
+#include "masterAccountFunctions.h"
+#include "credentialFunctions.h"
+#define PROJECT_NAME "TextPad"
+
+/* Prototypes for all the test functions */
+
+/********************************
+ * Filename -> passwordUsernameFormatChecker.c
+*/
+
+
+/********************************
+ * Filename -> masterAccountFunctions.c
+*/
+
+/********************************
+ * Filename -> credentialFunctions.c
+*/
+
+/* Required by the unity test framework */
+void setUp()
+{
+}
+/* Required by the unity test framework */
+void tearDown() {}
+
+
+/* Start of the application test */
+int main()
+{
+  /* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+  /* Run Test functions */
+
+  /********************************
+ * Filename -> passwordUsernameFormatChecker.c
+ */
+
+  /********************************
+ * Filename -> masterAccountFunctions.c
+*/
+
+  /********************************
+ * Filename -> credentialFunctions.c
+*/
+  
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */
+
+/********************************
+ * Filename -> passwordUsernameFormatChecker.c
+*/
